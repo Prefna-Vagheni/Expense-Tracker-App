6eb838e0dced860a8d526f95967fb9f71d1ec1cb
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-12 23:45:17 +0200

    JS setting up.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-import 'core-js/stable';
+'use strict';
 
-import 'regenerator-runtime/runtime';
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submit--btn');
+const addNewRow = document.querySelector('.add--row');
+
+addNewRow.addEventListener('click', function () {
+  form.classList.toggle('hidden');
+});

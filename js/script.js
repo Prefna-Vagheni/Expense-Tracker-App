254330df6c71807c14eaa2a446fec91be71c7412
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-13 21:27:12 +0200

    Submit button implemented

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ const form = document.querySelector('form');
 const submitBtn = document.querySelector('.submit--btn');
 const addNewRow = document.querySelector('.add--row');
 
+console.log(form);
+
 addNewRow.addEventListener('click', function () {
-  form.classList.toggle('hidden');
+  form.classList.remove('hidden');
+});
+
+submitBtn.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(form);
+
+  form.classList.add('hidden');
 });

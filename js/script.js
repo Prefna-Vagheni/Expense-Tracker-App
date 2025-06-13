95ae259bf777bb3c02f158cbf8ecc4549b986ba1
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-13 22:12:07 +0200

    Inserted html programatically

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('.submit--btn');
 const addNewRow = document.querySelector('.add--row');
+const tbody = document.querySelector('tbody');
 
 console.log(form);
 
@@ -14,6 +15,7 @@ submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
   console.log(form);
 
+  const month = document.getElementById('month').value;
   const billValue = document.getElementById('bills--input').value;
   const subscriptionValue = document.getElementById(
     'subscriptions---input'
@@ -21,16 +23,37 @@ submitBtn.addEventListener('click', function (e) {
   const entertainmentValue = document.getElementById(
     'entertainment--input'
   ).value;
-  const foodValue = document.getElementById('food--drink--input');
-  const groceryValue = document.getElementById('grocery--input');
-  const healthValue = document.getElementById('health--input');
-  const otherValue = document.getElementById('other--input');
-  const shippingValue = document.getElementById('shipping--input');
-  const transValue = document.getElementById('transport--input');
-  const travelValue = document.getElementById('travel--input');
-  const businessValue = document.getElementById('business--input');
-  const giftValue = document.getElementById('gift--input');
-  const totalValue = document.getElementById('total--input');
+  const foodValue = document.getElementById('food--drink--input').value;
+  const groceryValue = document.getElementById('grocery--input').value;
+  const healthValue = document.getElementById('health--input').value;
+  const otherValue = document.getElementById('other--input').value;
+  const shippingValue = document.getElementById('shipping--input').value;
+  const transValue = document.getElementById('transport--input').value;
+  const travelValue = document.getElementById('travel--input').value;
+  const businessValue = document.getElementById('business--input').value;
+  const giftValue = document.getElementById('gift--input').value;
+  const totalValue = document.getElementById('total--input').value;
+
+  const html = `
+    <tr>
+        <td class="month">${month}</td>
+        <td class="bill">${billValue}</td>
+        <td class="subscriptions">${subscriptionValue}</td>
+        <td class="entertainment">${entertainmentValue}</td>
+        <td class="food--drink">${foodValue}</td>
+        <td class="grocery">${groceryValue}</td>
+        <td class="health--wealth">${healthValue}</td>
+        <td class="other">${otherValue}</td>
+        <td class="shipping">${shippingValue}</td>
+        <td class="transport">${transValue}</td>
+        <td class="travel">${travelValue}</td>
+        <td class="business">${businessValue}</td>
+        <td class="gift">${giftValue}</td>
+        <td class="total--expense"></td>
+    </tr> 
+  `;
+
+  tbody.insertAdjacentHTML('afterbegin', html);
 
   console.log();
   form.classList.add('hidden');
